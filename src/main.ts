1f452a6176a1ec361874a41c6d2359d699bc2657
Jon Small <jonssmall@users.noreply.github.com>
2021-06-30 00:01:12 -0500

    basic use of TTY controls to output game results to console

--- src/main.ts
+++ src/main.ts
@@ -1,25 +1,24 @@
-// import { gameFactory, next } from "./engine";
-// import { Game } from "./models";
+import { gameFactory, next } from "./engine";
+import { Game } from "./models";
+import * as readline from "readline";
 
 // the display is every row joined by \n
 // and every element inside the row joined by whitespace,
 // where the element is either a full block or an empty space.
+function gameToDisplay(game: Game): string {
+  const ALIVE = "\u25A0";
+  const DEAD = "\u25A1";
 
-// function gameToDisplay(game: Game): string {
-//   const ALIVE = "\u25A0";
-//   const DEAD = "\u25A1";
-
-//   return game.map((r) => r.map((c) => (c ? ALIVE : DEAD)).join(" ")).join("\n");
-// }
-
-// let game = gameFactory(25, 25);
-
-// eslint-disable-next-line no-constant-condition
-while (true) {
-  console.log("hi");
-  // setInterval(() => {
-  //   game = next(game);
-  //   // process.stdout.write(gameToDisplay(game) + "\n");
-  //   console.log("hi");
-  // }, 500);
+  return game.map((r) => r.map((c) => (c ? ALIVE : DEAD)).join(" ")).join("\n");
 }
+
+let game = gameFactory(15, 15);
+
+setInterval(() => {
+  game = next(game);
+
+  readline.cursorTo(process.stdout, 0, 0);
+  readline.clearScreenDown(process.stdout);
+
+  process.stdout.write(gameToDisplay(game) + "\n");
+}, 100);
